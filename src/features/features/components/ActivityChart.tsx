3aeec7f75c92fa50214e859bb9857727fcb65bb5
Cauteros974 <pusik6666@ukr.net>
2025-12-28 15:31:17 +0200

    Create ActivityChart.tsx

---
+++ src/features/features/components/ActivityChart.tsx
@@ -0,0 +1,7 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {time: '00:00', count: 10},
+    {time: '04:00', count: 5},
+    {time: '08:00', count: 18}
+]
