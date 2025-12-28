bfc6ceb80ad1401c164bbcf1d31a86ff940fce58
Cauteros974 <pusik6666@ukr.net>
2025-12-28 15:52:07 +0200

    Update StatCard.tsx

--- src/features/features/components/StatCard.tsx
+++ src/features/features/components/StatCard.tsx
@@ -7,4 +7,10 @@ interface StatCardProps {
     color: string;
 }
 
-export const StartCard = ({title, value, icon: Icon, color} )
+export const StatCard = ({ title, value, icon: Icon, color }: StatCardProps) => (
+    <div className="bg-panel p-6 rounded-xl border border-state-800 flex items-center gap-5">
+        <div className={`p-3 rounded-lg ${color} bg-opacity-10`}>
+            <Icon className={color.replace('bg-', 'text-')} size={20}/>
+        </div>
+    </div>
+)
