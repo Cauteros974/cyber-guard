50edf04ee6c4478775c0454bd22843c778de815b
Cauteros974 <pusik6666@ukr.net>
2025-12-29 23:16:55 +0200

    Update DashboardPage.tsx

--- src/features/DashboardPage.tsx
+++ src/features/DashboardPage.tsx
@@ -18,7 +18,7 @@ export const DashboardPage = () => {
                 <StatCard title="Policy Compliance" value="98.2%" icon={ShieldCheck} color="text-green-500 bg-green-500" />
             </div>
 
-            <div className="grid grid-cols-1 gap-6">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
                 {/*Main Chart*/}
                 <div className="lg:col-span-2">
                     <ActivityChart />
