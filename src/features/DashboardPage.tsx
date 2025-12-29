574a2aa9cee3ef75ecf1b923944231571677d37d
Cauteros974 <pusik6666@ukr.net>
2025-12-29 23:13:41 +0200

    Update DashboardPage.tsx

--- src/features/DashboardPage.tsx
+++ src/features/DashboardPage.tsx
@@ -12,7 +12,8 @@ export const DashboardPage = () => {
 
             {/*Cards*/}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                <StatCard title="Active Incidents" value={11} icon={ShieldCheck} color="text-red-500 bg-red-500" />
+                <StatCard title="Active Incidents" value={12} icon={ShieldAlert} color="text-red-500 bg-red-500" />
+                <StatCard title="Critical Threats" value={3} icon={Zap} color="text-orange-500" />
             </div>
         </div>
     )
