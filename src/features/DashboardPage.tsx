2b5c844c29233b3af221642c2a381835b9bd2dad
Cauteros974 <pusik6666@ukr.net>
2025-12-29 23:12:46 +0200

    Update DashboardPage.tsx

--- src/features/DashboardPage.tsx
+++ src/features/DashboardPage.tsx
@@ -9,6 +9,11 @@ export const DashboardPage = () => {
                 <h1 className="text-2xl font-bold">Security Overview</h1>
                 <p className="text-slate-400">Real-time threat monitoring and system health.</p>
             </header>
+
+            {/*Cards*/}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                <StatCard title="Active Incidents" value={11} icon={ShieldCheck} color="text-red-500 bg-red-500" />
+            </div>
         </div>
     )
 }
