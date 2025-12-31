486670d4bbf5df29c0c1ca5f7d25c82d569e8c1f
Cauteros974 <pusik6666@ukr.net>
2025-12-31 18:30:08 +0200

    Create IncidentsPage.tsx

---
+++ src/features/incidents/IncidentsPage.tsx
@@ -0,0 +1,20 @@
+import { useMemo } from "react";
+import { useReactTable, getCoreRowModel, flexRender, createColumnHelper } from '@tanstack/react-table'
+import { useIncidentStore } from "../../store/useIncidentStore";
+import { type Incident } from "../../types/incident";
+import { SeverityBadge } from "./components/SeverityBadge";
+
+
+const columnHelper = createColumnHelper<Incident>();
+
+export const IncidentsPage = () => {
+    const {incidents} = useIncidentStore();
+
+    const columns = useMemo() => [
+        columnHelper.accessor('id', { header: 'ID', cell: info => <span className="text-slate-500 font-mono">{info.getValue()}</span> }),
+        columnHelper.accessor('severity', {
+            header: 'Severity',
+            cell: info => <SeverityBadge level={info.getValue()} />
+        }
+    ]
+}
