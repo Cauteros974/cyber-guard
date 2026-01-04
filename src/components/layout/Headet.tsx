236147320c1e4930af17b9ec6cf159fc2a138426
Cauteros974 <pusik6666@ukr.net>
2026-01-04 14:15:52 +0200

    Update Headet.tsx

--- src/components/layout/Headet.tsx
+++ src/components/layout/Headet.tsx
@@ -6,6 +6,15 @@ export const Header = () => {
         <header className="header">
             <div className="search-bar">
                 <Search size={18} color = "var(--text-muted)" />
+                <input type="text" placeholder="Search incidents, IPs or devices..."/>
+            </div>
+
+            <div className="header-actions">
+                <div className="notification-badge">
+                    <Bell size={18}>
+                        <span className="dot"></span>
+                    </Bell>
+                </div>
             </div>
         </header>
     )
