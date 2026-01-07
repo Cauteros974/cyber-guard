1ba36087bf6429986a102d9286bf8cdea60c6f95
Cauteros974 <pusik6666@ukr.net>
2026-01-07 22:17:24 +0200

    Update useIncidentStore.ts

--- src/store/useIncidentStore.ts
+++ src/store/useIncidentStore.ts
@@ -5,12 +5,10 @@ import { MOCK_INCIDENTS } from '../data/mockIncidents';
 interface IncidentState {
   incidents: Incident[];
   setIncidents: (incidents: Incident[]) => void;
+ 
 }
 
 export const useIncidentStore = create<IncidentState>((set) => ({
   incidents: MOCK_INCIDENTS,
-  addIncident: (newIncident) => 
-    set((state) => ({ 
-      incidents: [newIncident, ...state.incidents] 
-    })),
+  setIncidents: (incidents) => set({ incidents }),
 }));
