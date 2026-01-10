36a297de4909f5b9de1d21af7e52926dd3b239fb
Cauteros974 <pusik6666@ukr.net>
2026-01-10 19:22:15 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MainLayout } from './components/layout/MainLayout';
 import { DashboardPage } from './features/DashboardPage';
 import { IncidentsPage } from './features/incidents/IncidentsPage';
-
+import { useAttackSimulator } from './hooks/useAttackSimulator';
 
 function App() {
+  useAttackSimulator();
+  
   return (
     <Router>
       <MainLayout>
         <Routes>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/incidents" element={<IncidentsPage />} />
-          
           
           <Route path="*" element={<div className="p-10 text-center">Page Under Construction</div>} />
         </Routes>
