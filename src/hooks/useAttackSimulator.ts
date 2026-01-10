ae07896a26150846db4bc2f444d4514dbe9fcbf5
Cauteros974 <pusik6666@ukr.net>
2026-01-10 18:37:49 +0200

    Update useAttackSimulator.ts

--- src/hooks/useAttackSimulator.ts
+++ src/hooks/useAttackSimulator.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useIncidentStore } from "../store/useIncidentStore";
-import { Severity, type Incident} from "../features/incidents/IncidentsPage";
+import { type Severity, type Incident} from "../features/incidents/IncidentsPage";
 
 const SEVERITY: Severity[] = ['low', 'medium', 'height', 'critical'];
 const TITLES: ['Unauthorized Access Attempt', 'Malware DNS Beaconing '];
@@ -17,6 +17,10 @@ export const useAttackSimulator = () => {
                     title: TITLES[Math.floor(Math.random() * TITLES.length)],
                     severity: SEVERITY[Math.floor(Math.random() * TITLES.length)],
                     status: 'open',
+                    tactic: 'Initial Access',
+                    technique: 'T1109',
+                    source: `192.168.1.${Math.floor(Math.random() * 255)}`,
+                    timestamp: new Date().toISOString,
                 }
             }
         })
