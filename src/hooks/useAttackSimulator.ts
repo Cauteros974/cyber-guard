ee038e895082b521e0297376d64f7daf8ced1f68
Cauteros974 <pusik6666@ukr.net>
2026-01-10 18:29:50 +0200

    Update useAttackSimulator.ts

--- src/hooks/useAttackSimulator.ts
+++ src/hooks/useAttackSimulator.ts
@@ -6,5 +6,16 @@ const SEVERITY: Severity[] = ['low', 'medium', 'height', 'critical'];
 const TITLES: ['Unauthorized Access Attempt', 'Malware DNS Beaconing '];
 
 export const useAttackSimulator = () => {
-    const addIncident = useIncidentStore((state)) => state.addIncident
+    const addIncident = useIncidentStore((state) => state.addIncident);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            //Chance of attack occurrence(e.g 20% every 10 sec)
+            if(Math.random() > 0.8) {
+                const newIncident: Incident = {
+                    id: `INC-${Math.floor(Math.random())}`
+                }
+            }
+        })
+    })
 }
