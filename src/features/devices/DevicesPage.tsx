2d542c58a9f87d49bfc1ae90dd9c24427f1a3570
Cauteros974 <pusik6666@ukr.net>
2026-01-10 19:28:29 +0200

    Update DevicesPage.tsx

--- src/features/devices/DevicesPage.tsx
+++ src/features/devices/DevicesPage.tsx
@@ -2,5 +2,5 @@ import { Server, Monitor, Laptop, CheckCircle, AlertCircle } from "lucide-react"
 
 const devices = [
     {id: 'SRV-01', name: 'Main DB Cluster', type: 'server', status: 'online', cpu: 45, ram: 62},
-    {id: 'SRV-01'}
+    {id: 'SRV-01', name: 'Web Gateway', type: 'server', status: 'online', cpu: 12, ram: 30}
 ]
