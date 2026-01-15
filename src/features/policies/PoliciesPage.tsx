93287e41de3490da1fad950380317026c6ff308e
Cauteros974 <pusik6666@ukr.net>
2026-01-15 13:09:24 +0200

    Update PoliciesPage.tsx

--- src/features/policies/PoliciesPage.tsx
+++ src/features/policies/PoliciesPage.tsx
@@ -32,7 +32,7 @@ export const PoliciesPage = () => {
                     onClick={addPolicy}
                     className="bg-accent hover:big-blue-600 text-white px-4 py-2 rounded-lg flex"
                 >
-                    <Plus size={15} />
+                    <Plus size={18} /> Create Policy
                 </button>
             </header>
         </div>
