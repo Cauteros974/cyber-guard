ac6b002500f235e43a9eac722f708887ac2698a4
Cauteros974 <pusik6666@ukr.net>
2026-01-17 18:27:13 +0200

    Update useTheme.ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -6,3 +6,11 @@ interface ThemeState {
     toggleTheme: () => void;
 }
 
+export const useTheme = create <ThemeState>() (
+    persist(
+        (set) => ({
+            isDark: true;
+            toggleTheme: () => set((state) => ({isDark: !state.isDark})),
+        })
+    )
+)
