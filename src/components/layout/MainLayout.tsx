e1240987c12763cf9b46b3e7ac4815495a298e01
Cauteros974 <pusik6666@ukr.net>
2026-01-20 09:55:25 +0200

    commit

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -6,16 +6,16 @@ interface Props {
     children: React.ReactNode;
 }
 
-export const MainLayout = ({ children }: Props) => {
-    return (
-      <div className="layout">
-        <Sidebar />
-        <div className="main-container">
-          <Header />
-          <main className="content-area">
-            {children}
-          </main>
-        </div>
+export const MainLayout = ({ children }: {children: React.ReactNode}) =>{
+  return (
+    <div className="layout">
+      <Sidebar />
+      <div className="main-container">
+        <Header />
+        <main className="content-area">
+          {children}
+        </main>
       </div>
-    );
-  };
+    </div>
+  );
+};
