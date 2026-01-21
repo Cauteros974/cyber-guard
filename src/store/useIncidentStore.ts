5e801aaf792ad38b95336d5297affabbfcb5ee92
Cauteros974 <pusik6666@ukr.net>
2026-01-21 14:07:15 +0200

    Update useIncidentStore.ts

--- src/store/useIncidentStore.ts
+++ src/store/useIncidentStore.ts
@@ -1,26 +1,33 @@
 import { create } from 'zustand';
-import { type Incident } from '../types/incident';
+import { type Incident, type CreateIncidentDTO } from '../types/incident';
 import { MOCK_INCIDENTS } from '../data/mockData';
 
 interface IncidentState {
   incidents: Incident[];
-  setIncidents: (incidents: Incident[]) => void;
-  addIncident: (incident: Incident) => void;
   searchQuery: string;
+
   setSearchQuery: (query: string) => void;
+  setIncidents: (incidents: Incident[]) => void;
+  addIncident: (incident: CreateIncidentDTO) => void;
 }
 
 export const useIncidentStore = create<IncidentState>((set) => ({
   incidents: MOCK_INCIDENTS,
+  searchQuery: '',
 
-  setIncidents: (incidents) =>
-    set({ incidents }),
+  setSearchQuery: (query) => set({ searchQuery: query }),
+
+  setIncidents: (incidents) => set({ incidents }),
+
   addIncident: (incident) =>
     set((state) => ({
-      incidents: [incident, ...state.incidents],
+      incidents: [
+        {
+          ...incident,
+          technique: 'T1078',
+          source: 'Manual Report',
+        },
+        ...state.incidents,
+      ],
     })),
-
-  searchQuery: '',
-  setSearchQuery: (query) =>
-    set({ searchQuery: query }),
 }));
