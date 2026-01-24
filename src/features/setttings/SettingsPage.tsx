2c6c54da3968c7d5418d2927bfc4e4e84c576c5b
Cauteros974 <pusik6666@ukr.net>
2026-01-24 19:16:57 +0200

    Update SettingsPage.tsx

--- src/features/setttings/SettingsPage.tsx
+++ src/features/setttings/SettingsPage.tsx
@@ -6,6 +6,15 @@ export const SettingPage = () => {
     return(
         <div className="setting-container">
             <h1 className="page-title">Settings</h1>
+
+            <div className="settings-grid">
+                {/*Setting Navigation*/}
+                <div className="setting-nav">
+                    <button className="nav-item active"><User size={18}/>Profile</button>
+                    <button className="nav-item"><Monitor size={18}/> Appearance</button>
+                    <button className="nav-item"><Shield size={18}/> Security</button>
+                </div>
+            </div>
         </div>
     )
 }
