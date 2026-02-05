6219249692cf1472f9af8cd33c076abbf43d0475
Cauteros974 <pusik6666@ukr.net>
2026-02-05 22:23:51 +0200

    Update main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import random
+from pydantic import BaseModel
+from typing import List
+import datetime
 
 app = FastAPI()
 
-app.add_middleware = [
+app.add_middleware (
     CORSMiddleware,
-    allow_origins = ["*"],
+    allow_origins = ["*"], #Fake address
     allow_methods = ["*"],
+    allow_headers=["*"],
+)
+
+class Incident(BaseModel):
+    id: str
+    title: str
+    tactic: str
+    status: str
+
+dn_incidents = [
+    {
+        "id": "INC-8821",
+        "title": "Brute Force on SSH",
+        "tactic": "Credential Access",
+        "status": "open"
+    }
 ]
