972723e51d7195e6483116b64efda922426b06b7
Cauteros974 <pusik6666@ukr.net>
2026-02-06 19:51:23 +0200

    Update main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -56,4 +56,5 @@ async def calculate_risk():
     return{
         "score": min(100)
         weights = {"critical": 10, "high": 5, "medium": 2, "low": 1}
+        score = sum(weights.get(inc["severity"], 0) for inc in db_incidents)
     }
