99ca7a08d13bec1f28451ee005ec292c76a61391
Cauteros974 <pusik6666@ukr.net>
2026-02-15 17:47:46 +0200

    Update MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,4 +5,19 @@ import "./Layout.css";
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsSidebarOpen(prev => !prev);
+  };
+
+  return(
+    <div className="layout">
+       <Sidebar isOpen={isSidebarOpen} />
+
+       <div className="main-container">
+        <Header onMenuClick={handleMenuClick} />
+        <main className="content-area">{children}</main>
+       </div>
+    </div>
+  )
 }
