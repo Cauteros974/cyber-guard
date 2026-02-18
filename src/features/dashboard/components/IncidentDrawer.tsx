23e7190097038059a429e37f8b6ea9b6a0695bc2
Cauteros974 <pusik6666@ukr.net>
2026-02-18 19:00:41 +0200

    Update IncidentDrawer.tsx

--- src/features/dashboard/components/IncidentDrawer.tsx
+++ src/features/dashboard/components/IncidentDrawer.tsx
@@ -1,4 +1,4 @@
-import { Clock, X, Globe } from "lucide-react";
+import { Clock, X, Globe, ShieldAlert, Terminal } from "lucide-react";
 import { useIncidentStore } from "../../../store/useIncidentStore";
 import './IncidentDrawer.css';
 import { useState } from "react";
@@ -9,11 +9,15 @@ export default IncidentDrawer = () => {
     if(!selectedIncident) return null;
 
     return(
-        <div className="drawer">
+        <div className="{`incident-drawer ${selectedIncident ? 'is-open' : ''}`}">
             <div className="drawer-header">
                 <div className="drawer-title">
+                    <ShieldAlert size={20} className={selectedIncident.severity} />
                     <span>Incident Details: {selectedIncident.id}</span>
                 </div>
+                <button onClick={() => setSelectedIncident(null)}>
+                    <X size={20} />
+                </button>
             </div>
         </div>
     )
