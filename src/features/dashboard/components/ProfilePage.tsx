b0ce2013ddad5e9961a11a292537d2622d6b59c5
Cauteros974 <pusik6666@ukr.net>
2026-02-21 14:17:11 +0200

    Update ProfilePage.tsx

--- src/features/dashboard/components/ProfilePage.tsx
+++ src/features/dashboard/components/ProfilePage.tsx
@@ -5,4 +5,16 @@ export const ProfilePage = () => {
     const {user} = useUserStore();
 
     if(!user) return <div>Loading access credentials...</div>;
+
+    return(
+        <div className="profile-container">
+            <div className="profile-card">
+                <div className="profile-haeder">
+                    <div className="header-info">
+                        <h1>{user.username}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
