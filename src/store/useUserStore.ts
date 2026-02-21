079adca470e6ae0e7206c5a95bfd35986df6e310
Cauteros974 <pusik6666@ukr.net>
2026-02-21 14:14:21 +0200

    Update useUserStore.ts

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -18,7 +18,7 @@ interface UserStore{
 export const useUserStore = create<UserStore>((set)=> ({
     user: null,
     fetchUser: async() => {
-        const response = await fetch('http://127.0.0.1:8002/user/me');
+        const response = await fetch('http://127.0.0.1:8002/user/me'); //Don't forget about your port
         const data = await response.json();
         set({ user: data});
     }
