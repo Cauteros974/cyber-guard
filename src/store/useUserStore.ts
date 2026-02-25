883022ad322879d72d7351339ed0b132123a8873
Cauteros974 <pusik6666@ukr.net>
2026-02-25 18:49:59 +0200

    commit

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -9,4 +9,17 @@ interface UserProfile{
 interface UserStore{
     user: UserProfile | null;
     fetchUser: () => Promise<void>
+<<<<<<< Updated upstream
 }
+=======
+}
+
+export const useUserStore = create<UserStore>((set)=> ({
+    user: null,
+    fetchUser: async() => {
+        const response = await fetch('http://127.0.0.1:8010/user/me'); //Don't forget about your port
+        const data = await response.json();
+        set({ user: data});
+    }
+}));
+>>>>>>> Stashed changes
