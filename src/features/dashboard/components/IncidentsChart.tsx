7206d519da23accbe8f99f7d7bbddd1514a8db9e
Cauteros974 <pusik6666@ukr.net>
2026-02-27 12:12:40 +0200

    Update IncidentsChart.tsx

--- src/features/dashboard/components/IncidentsChart.tsx
+++ src/features/dashboard/components/IncidentsChart.tsx
@@ -16,7 +16,7 @@ export const IncidentsChart = () => {
     }, []);
 
     return(
-        <div style={{width: '100%', height: 300, padding: '20px', borderRadius: '12px', background: 'rgba(15, 23, 42, 0.5)'}}>
+        <div style={{width: '200%', height: 400, padding: '20px', borderRadius: '12px', background: 'rgba(12, 25, 56, 0.5)'}}>
             <h3 style={{ color: '#94A3B8', marginBottom: '20px', fontSize: '14px'}}>ATTACK VELOCITY (24H)</h3>
             <ResponsiveContainer width="100%" height="100%">
                 <AreaChart>
