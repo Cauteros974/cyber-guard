76f846512113e42b67568d3375370d19d3b139db
Cauteros974 <pusik6666@ukr.net>
2026-02-27 12:07:28 +0200

    Update IncidentsChart.tsx

--- src/features/dashboard/components/IncidentsChart.tsx
+++ src/features/dashboard/components/IncidentsChart.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
+interface IncidentStat {
+    time: string;
+    count: number;
+}
+
 export const IncidentsChart = () => {
-    const[data, setData] = useState([]);
+    const [data, setData] = useState<IncidentStat[]>([]);
 
     useEffect(() => {
         fetch('http://127.0.0.1:8000/incidents/stats')
-        .then(res => res.json)
+        .then(res => res.json())
         .then(json => setData(json));
     }, []);
 
