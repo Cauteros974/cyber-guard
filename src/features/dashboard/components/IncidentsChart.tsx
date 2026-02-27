0b1a02898c594b2f0fe165b202435682d4355a1b
Cauteros974 <pusik6666@ukr.net>
2026-02-27 11:32:29 +0200

    Update IncidentsChart.tsx

--- src/features/dashboard/components/IncidentsChart.tsx
+++ src/features/dashboard/components/IncidentsChart.tsx
@@ -8,5 +8,11 @@ export const IncidentsChart = () => {
         fetch('http://127.0.0.1:8000/incidents/stats')
         .then(res => res.json)
         .then(json => setData(json));
-    }, [])
+    }, []);
+
+    return(
+        <div style={{width: '100%', height: 300, padding: '20px', borderRadius: '12px'}}>
+            <h3 style={{ color: '#94A3B8', marginBottom: '20px'}}>ATTACK VELOCITY (24H)</h3>
+        </div>
+    )
 }
