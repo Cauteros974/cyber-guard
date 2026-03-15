219e6ff6b8a06489fd39d920d860f855870b79f8
Cauteros974 <pusik6666@ukr.net>
2026-03-15 21:53:50 +0200

    Update ProcessMonitor.jsx

--- src/features/dashboard/ProcessMonitor.jsx
+++ src/features/dashboard/ProcessMonitor.jsx
@@ -3,9 +3,9 @@ import { Activity, ShieldCheck, ShieldAlert, ShieldQuestion } from "lucide-react
 
 export const ProcessMonitor = () => {
     const process = [
-        {name: "chrome.exe"},
-        {name: "svchost.exe"},
-        {name: "unknown_proc_88.tmp"},
-        {name: "miner_script.js"},
+        {name: "chrome.exe", trust: 98, status: "Trusted", icon: ShieldCheck, color: "#22c55e"},
+        {name: "svchost.exe", trust: 100,},
+        {name: "unknown_proc_88.tmp", trust: 42,},
+        {name: "miner_script.js", trust: 12,},
     ];
 }
