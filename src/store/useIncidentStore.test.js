b6d9e7fdeae660092334ffcaaeb984d1edc0e9ee
Cauteros974 <pusik6666@ukr.net>
2026-03-27 20:48:32 +0200

    Update useIncidentStore.test.js

--- src/store/useIncidentStore.test.js
+++ src/store/useIncidentStore.test.js
@@ -1,8 +1,14 @@
-import { describe, beforeEach } from 'vitest';
+import { describe, beforeEach, expect } from 'vitest';
 import useIncidentStore from './useIncidentStore';
 
 describe ('Incident Store', () => {
     beforeEach(() => {
         const { setTheme } = useIncidentStore.getState();
-    })
+        setTheme('dark');
+    });
+
+    if('should initial theme as dark', () => {
+        const state = useIncidentStore.getState();
+        expect(state.theme).toBe('dark');
+    });
 })
