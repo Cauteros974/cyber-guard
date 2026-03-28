d9ea1037eada9d03c2b059916b1cb21b2e4bc250
Cauteros974 <pusik6666@ukr.net>
2026-03-28 19:10:30 +0200

    Update Settings.test.jsx

--- src/features/settings/Settings.test.jsx
+++ src/features/settings/Settings.test.jsx
@@ -19,6 +19,6 @@ describe('SettingPage Component', () => {
 
         render(<SettingPage />);
 
-        const themeSwitch = screen.getByText(/Interface Theme/)
+        const themeSwitch = screen.getByText(/Interface Theme/i)
     });
 });
